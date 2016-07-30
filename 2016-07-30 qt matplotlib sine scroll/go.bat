b442a47bf32b2964b634007dbd92caf7927fc966
Scott W Harden <swharden@gmail.com>
2016-07-30 19:27:09 -0400

    deleted files with spaces

--- 2016-07-30 qt matplotlib sine scroll/go.bat
+++
@@ -1,2 +0,0 @@
-python go.py
-pause
--- 2016-07-30 qt matplotlib sine scroll/ui_convert.py
+++
@@ -1,6 +0,0 @@
-from PyQt4 import uic 
-fin = open('ui_main.ui','r')
-fout = open('ui_main.py','w')
-uic.compileUi(fin,fout,execute=False)
-fin.close()
-fout.close()
